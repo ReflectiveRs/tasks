0c01e7abeef8c312005e6a605ec9a29a8f612187
Sergey Rus <silver256r@gmail.com>
2021-08-05 15:10:46 +0300

    add fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,6 @@
+let fibonacci = function(n) {        
+    let goldenRatio = 1.61803398874989484820;
+    return Math.round(Math.pow(goldenRatio, n) / Math.sqrt(5));
+}
+
+console.log(fibonacci(70));
